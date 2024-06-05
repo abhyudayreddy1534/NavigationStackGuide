c386d915abf31fe1ec25198339378add4ebf5507
Abhyuday Reddy Nandikonda <abhyudayreddy1534@gmail.com>
2024-06-05 11:10:01 -0500

    Initial Commit

---
+++ NavigationGuide/NavigationGuideApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationGuideApp.swift
+//  NavigationGuide
+//
+//  Created by Sravanthi Chinthireddy on 05/06/24.
+//
+
+import SwiftUI
+
+@main
+struct NavigationGuideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
